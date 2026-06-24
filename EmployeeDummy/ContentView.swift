3abb240930d45bba0134d5859d4a5a681f886e3c
saeed-mohammad <sk.saeed008@gmail.com>
2026-06-24 20:43:04 +0530

    Initial Commit

---
+++ EmployeeDummy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmployeeDummy
+//
+//  Created by saeed shaikh on 24/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmployeeDummy/EmployeeDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDummyApp.swift
+//  EmployeeDummy
+//
+//  Created by saeed shaikh on 24/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
